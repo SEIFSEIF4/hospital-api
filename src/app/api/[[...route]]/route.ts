b66eb216b680093b02713fc1b0fecd29104e80fb
Seif Elesllam <seifsaif151@gmail.com>
2024-05-01 13:02:37 +0300

    rate-limiter

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,12 +1,53 @@
 import { Redis } from "@upstash/redis/cloudflare";
-import { Hono } from "hono";
+import { Context, Hono } from "hono";
 import { env } from "hono/adapter";
 import { cors } from "hono/cors";
 import { handle } from "hono/vercel";
+import { Ratelimit } from "@upstash/ratelimit";
+import { BlankInput, Env } from "hono/types";
+
+declare module "hono" {
+  interface ContextVariableMap {
+    ratelimit: Ratelimit;
+  }
+}
+
+let RateLimitDuration = 1800;
+let MaxNumOfRequests = 50;
 
 export const runtime = "edge";
-
 const app = new Hono().basePath("/api");
+const cache = new Map();
+
+class RateLimiter {
+  static instances: Ratelimit;
+
+  static getInstance(c: Context<Env, any, BlankInput>) {
+    if (!this.instances) {
+      const { UPSTASH_REDIS_REST_TOKEN, UPSTASH_REDIS_REST_URL } =
+        env<EnvConfig>(c);
+
+      const redisClient = new Redis({
+        token: UPSTASH_REDIS_REST_TOKEN,
+        url: UPSTASH_REDIS_REST_URL,
+      });
+
+      const ratelimit = new Ratelimit({
+        redis: redisClient,
+        limiter: Ratelimit.slidingWindow(
+          MaxNumOfRequests,
+          `${RateLimitDuration} s`
+        ),
+        ephemeralCache: cache,
+      });
+
+      this.instances = ratelimit;
+      return this.instances;
+    } else {
+      return this.instances;
+    }
+  }
+}
 
 type EnvConfig = {
   UPSTASH_REDIS_REST_TOKEN: string;
@@ -15,57 +56,77 @@ type EnvConfig = {
 
 app.use("/*", cors());
 
+app.use(async (c, next) => {
+  const ratelimit = RateLimiter.getInstance(c);
+  c.set("ratelimit", ratelimit);
+  await next();
+});
+
 app.get("/search", async (c) => {
-  try {
-    const { UPSTASH_REDIS_REST_TOKEN, UPSTASH_REDIS_REST_URL } =
-      env<EnvConfig>(c);
+  const ratelimit = c.get("ratelimit");
+  const ip = c.req.raw.headers.get("CF-Connecting-IP");
 
-    const start = performance.now();
-    // ---------------------
+  const { success } = await ratelimit.limit(ip ?? "anonymous");
 
-    const redis = new Redis({
-      token: UPSTASH_REDIS_REST_TOKEN,
-      url: UPSTASH_REDIS_REST_URL,
-    });
+  if (success) {
+    try {
+      const { UPSTASH_REDIS_REST_TOKEN, UPSTASH_REDIS_REST_URL } =
+        env<EnvConfig>(c);
 
-    const query = c.req.query("q")?.toUpperCase();
+      const start = performance.now();
+      // ---------------------
 
-    if (!query) {
-      return c.json({ message: "Invalid search query" }, { status: 400 });
-    }
+      const redis = new Redis({
+        token: UPSTASH_REDIS_REST_TOKEN,
+        url: UPSTASH_REDIS_REST_URL,
+      });
 
-    const res = [];
-    const rank = await redis.zrank("Hterms", query);
+      const query = c.req.query("q")?.toUpperCase();
 
-    if (rank !== null && rank !== undefined) {
-      const temp = await redis.zrange<string[]>("Hterms", rank, rank + 100);
+      if (!query) {
+        return c.json({ message: "Invalid search query" }, { status: 400 });
+      }
 
-      for (const el of temp) {
-        if (!el.startsWith(query)) {
-          break;
-        }
+      const res = [];
+      const rank = await redis.zrank("Hterms", query);
 
-        if (el.endsWith("*")) {
-          res.push(el.substring(0, el.length - 1));
+      if (rank !== null && rank !== undefined) {
+        const temp = await redis.zrange<string[]>("Hterms", rank, rank + 100);
+
+        for (const el of temp) {
+          if (!el.startsWith(query)) {
+            break;
+          }
+
+          if (el.endsWith("*")) {
+            res.push(el.substring(0, el.length - 1));
+          }
         }
       }
+
+      // ------------------------
+      const end = performance.now();
+
+      return c.json({
+        results: res,
+        duration: end - start,
+        MaxNumOfRequests: MaxNumOfRequests,
+        RateLimitDuration: RateLimitDuration,
+      });
+    } catch (err) {
+      console.error(err);
+
+      return c.json(
+        { results: [], message: "Something went wrong." },
+        {
+          status: 500,
+        }
+      );
     }
-
-    // ------------------------
-    const end = performance.now();
-
-    return c.json({
-      results: res,
-      duration: end - start,
-    });
-  } catch (err) {
-    console.error(err);
-
+  } else {
     return c.json(
-      { results: [], message: "Something went wrong." },
-      {
-        status: 500,
-      }
+      { message: "Rate limit exceeded. Try again after some time." },
+      { status: 429 }
     );
   }
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,13 +11,25 @@ import {
   CommandList,
 } from "@/components/ui/command";
 
+const apiMaxNumOfRequests = 50;
+
 export default function Home() {
   const { theme, setTheme } = useTheme();
+  const storedRequestCount = localStorage.getItem("requestCount");
+  const initialRequestCount = storedRequestCount
+    ? parseInt(storedRequestCount)
+    : apiMaxNumOfRequests;
+  const [requestCount, setRequestCount] = useState(initialRequestCount);
   const [input, setInput] = useState<string>("");
   const [searchResults, setSearchResults] = useState<{
     results: string[];
     duration: number;
+    MaxNumOfRequests: number;
   }>();
+
+  useEffect(() => {
+    localStorage.setItem("requestCount", requestCount.toString());
+  }, [requestCount]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -25,10 +37,15 @@ export default function Home() {
       // the prefix to test  this on cloudflare worker url is
       // // https://hospitalapi.onealking151.workers.dev/api/search?q=<input>
 
+      setRequestCount(requestCount - 1);
+      if (requestCount <= 1) return;
+      console.log("requestCount", requestCount);
+
       const res = await fetch(`/api/search?q=${input}`);
       const data = (await res.json()) as {
         results: string[];
         duration: number;
+        MaxNumOfRequests: number;
       };
       setSearchResults(data);
     };
@@ -38,12 +55,12 @@ export default function Home() {
 
   return (
     <main
-      className={`h-screen  w-screen select-none ${
-        theme === "dark" ? "" : "grainy"
+      className={`h-screen w-screen select-none ${
+        theme === "light" ? "grainy" : ""
       }`}
     >
-      <div className="flex flex-col gap-6 items-center pt-32 .grainy-content  duration-500 animate-in animate fade-in-5 slide-in-from-bottom-2.5">
-        <p className="text-zinc-500 text-sm font-bold">v1.0.0</p>
+      <div className="flex flex-col gap-6 items-center pt-32  duration-500 animate-in animate fade-in-5 slide-in-from-bottom-2.5">
+        <p className="text-primary-500 text-sm font-bold">v1.0.0</p>
         <h1
           onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
           className="text-5xl cursor-pointer tracking-tight text-primary font-bold"
@@ -51,19 +68,20 @@ export default function Home() {
           Hospital API ⚡
         </h1>
         <p className="text-primary-600 text-xs md:text-lg lg:text-xl  max-w-prose  text-center">
-          This an app with a REST API. The API is served by a serverless
+          This an App built with a REST API. The API is served by a serverless
           function.
         </p>
         <div className="max-w-md w-full">
           <Command>
             <CommandInput
               value={input}
+              disabled={requestCount === 0}
               onValueChange={setInput}
               placeholder="Search countries..."
               className="placeholder:text-primary-500"
             />
             <CommandList>
-              {searchResults?.results.length === 0 ? (
+              {searchResults?.results?.length === 0 ? (
                 <CommandEmpty>No results found.</CommandEmpty>
               ) : null}
 
@@ -84,11 +102,17 @@ export default function Home() {
               {searchResults?.results ? (
                 <>
                   <div className="h-px w-full bg-primary-200" />
-
-                  <p className="p-2 text-xs text-primary-500">
-                    Found {searchResults.results.length} results in{" "}
-                    {searchResults?.duration.toFixed(0)}ms
-                  </p>
+                  <div className="flex justify-between">
+                    <p className="p-2 text-xs text-primary-500">
+                      Found {searchResults.results.length} results in{" "}
+                      {searchResults?.duration.toFixed(0)}ms
+                    </p>
+                    <p className="p-2 text-xs text-primary-500">
+                      {apiMaxNumOfRequests - requestCount} API requests{" "}
+                      <span className="text-red-500">{requestCount}</span>{" "}
+                      remaining
+                    </p>
+                  </div>
                 </>
               ) : null}
             </CommandList>
